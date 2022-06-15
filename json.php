d0230690fb4f28628737a3bcd41d2b5089c3cde3
admin <admin@VIL-S01-iMac-ITA-23.local>
2022-06-15 12:48:55 +0200

    city name from json write in csv

--- json.php
+++ json.php
@@ -1,17 +1,19 @@
 <?php
 $cle = "77ee46c379fcc1190dac3507abd1afa0";
 $ville = ['LYON','PARIS','LONDON'];
-$ville2 = "LYON";
-
-
-// $url_api = "http://api.openweathermap.org/data/2.5/weather?q=LYON&appid=".$cle;
-// var_dump($url_api);
+$csv = "meteo.csv";
+$fp = fopen($csv, 'w');
+$array = [];
 
 foreach($ville as $v){
     $url_api = "http://api.openweathermap.org/data/2.5/weather?q=".$v."&appid=".$cle;
     $json = json_decode(file_get_contents($url_api));
-    var_dump($json->name);
+    // var_dump($json);
+    var_dump($json->name, $json->coord->lat, $json->coord->lon, $json->main->humidity, $json->main->temp, $json->main->pressure);
+    array_push($array,$json->name);
+    // fputcsv(fgetcsv($fp),$json);
+   
 }
 
-
+ fputcsv($fp,$array);
 ?>
