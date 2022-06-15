d56abadad56f3a3950e1ff5b2acd8cde794d91de
admin <admin@VIL-S01-iMac-ITA-23.local>
2022-06-15 12:18:46 +0200

    api requested

--- json.php
+++ json.php
@@ -1,11 +1,16 @@
 <?php
 $cle = "77ee46c379fcc1190dac3507abd1afa0";
 $ville = ['LYON','PARIS','LONDON'];
-$url_api = "http://api.openweathermap.org/data/2.5/weather?q=".$ville."&appid=".$cle;
+$ville2 = "LYON";
+
+
+// $url_api = "http://api.openweathermap.org/data/2.5/weather?q=LYON&appid=".$cle;
+// var_dump($url_api);
 
 foreach($ville as $v){
-    $json = file_get_contents($url_api);
-    var_dump($json);
+    $url_api = "http://api.openweathermap.org/data/2.5/weather?q=".$v."&appid=".$cle;
+    $json = json_decode(file_get_contents($url_api));
+    var_dump($json->name);
 }
 
 
