0072fc23bfa919f1b96f534db6c1bc2b3176a0dc
nicolas-nn <n.nguyen@it-students.fr>
2022-06-15 11:36:19 +0200

    links.csv

--- script_php_16_links.php
+++ script_php_16_links.php
@@ -3,23 +3,25 @@
 $csv = "links.csv";
 #avec https://www.google.com/ ca marche
 $site = file_get_contents('https://www.google.com/');
-
-// for($i=0; $i<strlen($site); $i++){
-//     if($site[$i]=='h'){
-//         echo $site[$i];
-//     }
-// }
-
-
+$data=[];
 // var_dump($csv);
 preg_match_all('#(https?://)([\w\d.&:\#@%/;$~_?\+\-=]*)#',$site,$matches);
-print_r($matches);
-foreach($matches as $k=>$match){
-    file_put_contents($csv,$match);
-    echo $match[0];
-    echo "\n";
-    echo $k;
-    echo "\n";
-    var_dump($match);
+// print_r($matches);
+
+foreach($matches[0] as $key=>$match){
+    // echo ;
+    // $data += $match."\n";
+    array_push($data,$match."\n");
+    // foreach($match as $k=>$value){
+    //     // file_put_contents($csv,$value);
+    //     echo $k.$value."\n";
+    //     // echo "\n";
+    // }
+    // file_put_contents($csv,$match);
+    // echo $match[0];
+    // echo "\n";
+    // echo $k;
+    // echo "\n";
+    // var_dump($match);
     
-}
+}file_put_contents($csv,$data);
